ddc71984c287e084baa893540587e4b43aa4bd01
chaharhimanshu1004 <chaharhimanshu03@gmail.com>
2024-09-17 02:12:20 +0530

    update/changed resend to nodemailer

--- helpers/sendVerificationEmail.ts
+++ helpers/sendVerificationEmail.ts
@@ -1,29 +1,7 @@
-// import { resend } from "@/lib/resend";
-import { NextResponse } from "next/server";
+
 import nodemailer from "nodemailer";
-// import VerificationEmail from "@/emails/verificationEmails";
 import { ApiResponse } from "@/types/ApiResponse";
 
-// export async function sendVerificationEmail(email: string, name: string, verificationCode: string):Promise<ApiResponse> {
-//     try{
-//         await resend.emails.send({
-//             from: 'onboarding@resend.dev',
-//             to: email,
-//             subject: 'Uptime Monitoring | Verification code',
-//             react: VerificationEmail({ name, otp: verificationCode }),
-//           });
-//         return {
-//             success: true,
-//             message: "Verifcation email send successfully, check your email!"
-//         }
-//     }catch(err){
-//         console.error("Error in sending email", err);
-//         return {
-//             success: false,
-//             message: "Failed to send verification email"
-//         }
-//     }
-// };
 
 export async function sendVerificationEmail(
   email: string,
