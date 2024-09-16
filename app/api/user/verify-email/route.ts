739e36053be0736656604cb4afaf448c40766f20
chaharhimanshu1004 <chaharhimanshu03@gmail.com>
2024-09-16 20:48:08 +0530

    feat/added verify-email

---
+++ app/api/user/verify-email/route.ts
@@ -0,0 +1,62 @@
+import prisma from "@/lib/prisma";
+
+export async function POST(request: Request) {
+  try {
+    const { email, code } = await request.json();
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) {
+      return Response.json(
+        {
+          success: false,
+          message: "No user found with the corresponding email",
+        },
+        {
+          status: 404,
+        }
+      );
+    }
+
+    const isCodeValid = user.verificationCode === code;
+    if (isCodeValid) {
+      await prisma.user.update({
+        where: {
+          email,
+        },
+        data: {
+          isVerified: true,
+        },
+      });
+
+      return Response.json({
+        success: true,
+        message: "Email verified successfully",
+      });
+    }
+
+    return Response.json(
+      {
+        success: false,
+        message: "Invalid code",
+      },
+      {
+        status: 400,
+      }
+    );
+  } catch (error: any) {
+    console.log("Error verifying code", error);
+    return Response.json(
+      {
+        success: false,
+        message: "Error verifying code",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
