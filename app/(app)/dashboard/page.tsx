3b33d02cf0d153e3db18613a0282e07fac87a47d
chaharhimanshu1004 <chaharhimanshu03@gmail.com>
2024-09-24 17:17:43 +0530

    update/user in the queue

--- app/(app)/dashboard/page.tsx
+++ app/(app)/dashboard/page.tsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import { useSession, signOut } from 'next-auth/react'
 import { User } from 'next-auth' // this is the user next-auth returns after the authentication
-
+import { WebsiteStatusDisplay } from '@/app/components/WebsiteStatusDisplay'
 const Page = () => {
   const { data: session } = useSession();
   const user: User = session?.user as User;
@@ -13,6 +13,7 @@ const Page = () => {
         <div className="text-center">
           <h1 className="text-4xl font-bold mb-6 text-gray-800">Dashboard</h1>
           <p className="text-lg text-gray-600 mb-4">Welcome {user?.name}</p>
+          <WebsiteStatusDisplay />
           <button
             className="px-6 py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition"
             onClick={() => signOut()}
--- lib/queue.ts
+++ lib/queue.ts
@@ -1,16 +1,29 @@
 import redis from "./redis";
 
 const QUEUE_NAME = "uptime-monitoring-queue";
+const USER_WEBSITES_SET = "user-websites:";
 
 interface WebsiteCheck {
   url: string;
   nextCheckTime: number;
+  userId: string;
 }
 
-export async function registerWebsite(url: string) {
+export async function registerWebsite(url: string,userId:string) {
   const initialCheckTime = Date.now();
-  await addWebsiteToQueue({ url, nextCheckTime: initialCheckTime });
+  // await addWebsiteToQueue({ url, nextCheckTime: initialCheckTime,userId });
+  const check: WebsiteCheck = { url, nextCheckTime: initialCheckTime, userId };
+  
+  await Promise.all([
+    addWebsiteToQueue(check),
+    addWebsiteToUserSet(userId, url)
+  ]);
 
+
+}
+
+async function addWebsiteToUserSet(userId: string, url: string) {
+  await redis.sadd(`${USER_WEBSITES_SET}${userId}`, url);
 }
 
 async function addWebsiteToQueue(check: WebsiteCheck) {
