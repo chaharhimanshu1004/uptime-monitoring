ea446c30a8989a07fdd4aa6b377b56f80639b987
chaharhimanshu1004 <chaharhimanshu03@gmail.com>
2024-09-25 13:46:10 +0530

    update/queue added email in the redis

--- lib/queue.ts
+++ lib/queue.ts
@@ -1,30 +1,29 @@
 import redis from "./redis";
 
 const QUEUE_NAME = "uptime-monitoring-queue";
-const USER_WEBSITES_SET = "user-websites:";
+// const USER_WEBSITES_SET = "user-websites:";
 
 interface WebsiteCheck {
   url: string;
   nextCheckTime: number;
   userId: string;
+  userEmail: string;
 }
 
-export async function registerWebsite(url: string,userId:string) {
+export async function registerWebsite(url: string,userId:string,userEmail:string) {
   const initialCheckTime = Date.now();
   // await addWebsiteToQueue({ url, nextCheckTime: initialCheckTime,userId });
-  const check: WebsiteCheck = { url, nextCheckTime: initialCheckTime, userId };
-  
-  await Promise.all([
-    addWebsiteToQueue(check),
-    addWebsiteToUserSet(userId, url)
-  ]);
-
-
+  const check: WebsiteCheck = { url, nextCheckTime: initialCheckTime, userId,userEmail };
+  await addWebsiteToQueue(check);
+  // await Promise.all([
+  //   addWebsiteToQueue(check),
+  //   addWebsiteToUserSet(userId, url)
+  // ]);
 }
 
-async function addWebsiteToUserSet(userId: string, url: string) {
-  await redis.sadd(`${USER_WEBSITES_SET}${userId}`, url);
-}
+// async function addWebsiteToUserSet(userId: string, url: string) {
+//   await redis.sadd(`${USER_WEBSITES_SET}${userId}`, url);
+// }
 
 async function addWebsiteToQueue(check: WebsiteCheck) {
   await redis.zadd(QUEUE_NAME, check.nextCheckTime, JSON.stringify(check));
