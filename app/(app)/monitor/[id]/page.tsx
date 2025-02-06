2b085c67326f339c272050289a64bc4f76914b9a
chaharhimanshu1004 <chaharhimanshu03@gmail.com>
2025-02-07 00:59:08 +0530

    adding charts

--- app/(app)/monitor/[id]/page.tsx
+++ app/(app)/monitor/[id]/page.tsx
@@ -1,20 +1,13 @@
 "use client"
-import { useParams } from "next/navigation";
-import { useEffect, useState } from 'react'
-import axios from 'axios'
+import WebsiteStats from "../website-stats";
 
 
-export default function WebsiteDetailsPage() {
-    const params = useParams();
-    const { id } = params;
-    // console.log('>>>id is here brother',id);
-    const [websiteDetails, setWebsiteDetails] = useState<any>(null);
-
-    if (!websiteDetails) return <div>Loading...</div>
-
+export default function WebsiteDetailsPage({ params }: { params: { websiteId: number } }) {
     return (
-        <div className="min-h-screen bg-[#0A0A0B] text-white p-6">
-            <h1 className="text-2xl font-bold mb-4">{websiteDetails.url}</h1>
+        <div>
+            <div className="container mx-auto py-8">
+                <WebsiteStats websiteId={params.websiteId} />
+            </div>
         </div>
-    )
+    );
 }
