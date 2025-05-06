6f0a764528b5f893aeceef5b9b576c2483bbe47c
chaharhimanshu1004 <chaharhimanshu03@gmail.com>
2025-05-07 00:14:02 +0530

    fix: middleware

--- middleware.ts
+++ middleware.ts
@@ -7,33 +7,34 @@ import { getToken } from 'next-auth/jwt'
 
 
 export async function middleware(request: NextRequest) {
-    const token = await getToken({ req: request })
-    const url = request.nextUrl;
-    
-    if( token && 
-      (
-        
-        url.pathname.startsWith('sign-in') || 
-        url.pathname.startsWith('sign-up') || 
-        url.pathname.startsWith('verify-email') 
-        
-    )
-     ){
-        return NextResponse.redirect(new URL('/dashboard', request.url));
-    }
-    if(!token && (url.pathname.startsWith('/dashboard') || url.pathname.startsWith('/add-website') )){
-        return NextResponse.redirect(new URL('/sign-in', request.url));
-    }
-    return NextResponse.next();
+  const token = await getToken({ req: request })
+  const url = request.nextUrl;
+
+  const alreadyLoggedIn = ['/onboarding', '/sign-up', '/verify-email', '/sign-in'];
+
+  const notLoggedIn = ['/dashboard', '/add-website', '/incidents', '/settings', '/monitor'];
+
+  if (token && alreadyLoggedIn.some(path => url.pathname.startsWith(path))) {
+    return NextResponse.redirect(new URL('/dashboard', request.url));
+  }
+  
+  if (!token && notLoggedIn.some(path => url.pathname.startsWith(path))) {
+    return NextResponse.redirect(new URL('/onboarding/sign-in', request.url));
+  }
+  return NextResponse.next();
 }
- 
+
 // See "Matching Paths" below to learn more
 export const config = {
   matcher: [
     '/onboarding/sign-in',
     '/onboarding/sign-up',
     '/onboarding/verify-email',
-    '/',
-    
+    '/dashboard',
+    '/monitor/:path*',
+    '/onboarding',
+    '/verify-email',
+    '/incidents',
+    '/settings',
   ]
 }
